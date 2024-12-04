9b4c357561d711bb7600972955e00afc96658e15
tchwojkofrank <tom@chwojkofrank.com>
2024-12-03 17:11:03 -0800

    Day 03 part 2

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -52,7 +52,70 @@ fn get_instruction_args(contents: &String) -> Vec<(i32, i32)> {
 
 #[allow(unused_variables)]
 pub fn part2(contents: &String) -> String {
-    2.to_string()
+    let instruction_args = get_full_instruction_args(contents);
+    let result = get_full_instruction_value(&instruction_args);
+    result.to_string()
+}
+
+enum Instruction {
+    IDo,
+    IDont,
+    IMul(i32, i32),
+}
+
+fn get_full_instruction_args(contents: &String) -> Vec<Instruction> {
+    let valid_instruction = Regex::new(r"(?<mul>mul)\((\d+),(\d+)\)|(?<do>do)\(\)|(?<dont>don't)\(\)").unwrap();
+    let mut instructions: Vec<Instruction> = Vec::new();
+    for instruction_capture in valid_instruction.captures_iter(contents) {
+        let instruction = instruction_capture.name("mul").or(instruction_capture.name("do")).or(instruction_capture.name("dont"));
+        let mut instruction_string = "";
+        match instruction {
+            None => {
+                println!("Invalid instruction");
+            },
+            Some(_) => {
+                instruction_string = instruction.unwrap().as_str();
+            },
+        }
+        match instruction_string {
+            "mul" => {
+                let arg1 = instruction_capture.get(2).unwrap().as_str().parse().unwrap();
+                let arg2 = instruction_capture.get(3).unwrap().as_str().parse().unwrap();
+                instructions.push(Instruction::IMul(arg1, arg2));
+            },
+            "do" => {
+                instructions.push(Instruction::IDo);
+            },
+            "don\'t" => {
+                instructions.push(Instruction::IDont);
+            },
+            _ => {
+                println!("Invalid instruction: {}", instruction_string);
+            },
+        }
+    }
+    instructions
+}
+
+fn get_full_instruction_value(instruction_args: &Vec<Instruction>) -> i32 {
+    let mut count = 0;
+    let mut enable = true;
+    for instruction in instruction_args {
+        match instruction {
+            Instruction::IMul(a, b) => {
+                if enable {
+                    count += a * b;
+                }
+            },
+            Instruction::IDo => {
+                enable = true;
+            },
+            Instruction::IDont => {
+                enable = false;
+            },
+        }
+    }
+    count
 }
 
 #[cfg(test)]
@@ -74,7 +137,7 @@ mod tests {
     #[test]
     fn test2() {
         // get the contents of the file "files/test"
-        let contents = advent::read_file_to_string("files/test");
+        let contents = advent::read_file_to_string("files/test2");
         // call part2 with the contents of the file
         let result = part2(&contents);
         // get the contents of the file "files/test_answer_2"
