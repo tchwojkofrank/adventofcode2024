9891cef83e6aa37a9ee45a345b63804c7489cca2
tchwojkofrank <tom@chwojkofrank.com>
2024-12-06 22:57:24 -0800

    Day 07 part 2

--- day07/src/main.rs
+++ day07/src/main.rs
@@ -31,16 +31,16 @@ pub fn part1(contents: &String) -> String {
     let lines = contents.split("\n").collect::<Vec<&str>>();
     let mut sum = 0;
     for line in lines {
-        sum += test_line(line);
+        sum += test_line(line,check_answer);
     }
     sum.to_string()
 }
 
-fn test_line(line: &str) -> i64 {
+fn test_line(line: &str, check_fn: fn(i64, Vec<i64>) -> bool) -> i64 {
     let parts = line.split(":").map(|x| x.trim()).collect::<Vec<&str>>();
     let answer = parts[0].parse::<i64>().unwrap();
     let operands = parts[1].split(" ").map(|x| x.parse::<i64>().unwrap()).collect();
-    if check_answer(answer, operands) {
+    if check_fn(answer, operands) {
         return answer;
     }
     0
@@ -60,9 +60,55 @@ fn check_answer(answer: i64, operands: Vec<i64>) -> bool {
     false
 }
 
+fn check_answer2(answer: i64, operands: Vec<i64>) -> bool {
+    if answer < 0 {
+        return false;
+    }
+    let length = operands.len();
+    if length == 1 {
+        return operands[0] == answer;
+    }
+    // a = (...) op b
+
+    // check if a = (...) + b, or a-b = (...)
+    if check_answer2(answer - operands[length-1], operands[0..length-1].to_vec()) {
+        return true;
+    }
+
+    // check if a = (...) * b, or a/b = (...)
+    if (answer % operands[length-1] == 0) && (check_answer2(answer / operands[length-1], operands[0..length-1].to_vec())) {
+        return true;
+    }
+
+    // check if a = (...)b, or a\b = (...)
+    let a_string = answer.to_string();
+    let b_string = operands[length-1].to_string();
+    // check if b_string is a suffix of a_string
+    if (a_string.len() > b_string.len()) && (a_string.ends_with(&b_string)) {
+        let new_a = a_string[0..a_string.len()-b_string.len()].to_string();
+        let a = new_a.parse::<i64>();
+        match a {
+            Ok(a) => {
+                if check_answer2(a, operands[0..length-1].to_vec()) {
+                    return true;
+                }
+            }
+            Err(_) => {
+                println!("Error parsing {} after removing {} from {} with {:?} remaining", new_a, b_string, a_string, operands[0..length-1].to_vec());
+            }
+        }
+    }
+    false
+}
+
 #[allow(unused_variables)]
 pub fn part2(contents: &String) -> String {
-    2.to_string()
+    let lines = contents.split("\n").collect::<Vec<&str>>();
+    let mut sum = 0;
+    for line in lines {
+        sum += test_line(line,check_answer2);
+    }
+    sum.to_string()
 }
 
 #[cfg(test)]
