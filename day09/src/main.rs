c258672b4b63c58efee0c24cd895e3e3b5d94314
tchwojkofrank <tom@chwojkofrank.com>
2024-12-08 21:06:41 -0800

    Day 09 setup

---
+++ day09/src/main.rs
@@ -0,0 +1,66 @@
+use std::time::Instant;
+
+// use the advent package
+use advent;
+fn main() {
+    let args = advent::get_commandline_arguments();
+    // the first argument is the input file name
+    // if the user does not provide a first argument, the length of args will be zero
+    if args.len() == 0 {
+        eprintln!("Usage: day00 <input file>");
+        std::process::exit(1);
+    }
+    // the contents of the file are ascii text. Read the contents of the file into a string
+    let filename = &args[0];
+    let contents = advent::read_file_to_string(filename);
+    // call part1 with the contents of the file
+    let start = Instant::now();
+    let result1 = part1(&contents);
+    let duration = start.elapsed();
+    println!("Part 1:\n{}\n\tTook {:?}", result1, duration);
+
+    let start = Instant::now();
+    let result2 = part2(&contents);
+    let duration = start.elapsed();
+    println!("Part 2:\n{}\n\tTook {:?}", result2, duration);
+}
+
+// turn off warning for unused variables
+#[allow(unused_variables)]
+pub fn part1(contents: &String) -> String {
+    1.to_string()
+}
+
+#[allow(unused_variables)]
+pub fn part2(contents: &String) -> String {
+    2.to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test1() {
+        // get the contents of the file "files/test"
+        let contents = advent::read_file_to_string("files/test");
+        // call part1 with the contents of the file
+        let result = part1(&contents);
+        // get the contents of the file "files/test_answer_1"
+        let answer = advent::read_file_to_string("files/test_answer_1");
+        // compare the result with the answer
+        assert_eq!(result, answer);
+    }
+
+    #[test]
+    fn test2() {
+        // get the contents of the file "files/test"
+        let contents = advent::read_file_to_string("files/test");
+        // call part2 with the contents of the file
+        let result = part2(&contents);
+        // get the contents of the file "files/test_answer_2"
+        let answer = advent::read_file_to_string("files/test_answer_2");
+        // compare the result with the answer
+        assert_eq!(result, answer);
+    }
+}
